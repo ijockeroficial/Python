8dda1bc8ae1eccd55cffeedd62f702304ca58398
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-13 18:15:44 -0300

    Função: Linhas do tamanho do texto

---
+++ CursoEmVídeo/Mundo 3/FUNÇÕES/linhas.py
@@ -0,0 +1,9 @@
+def mensagem(msg):
+    tam = len(msg)
+    print("_" * tam)
+    print(msg)
+
+
+mensagem("Olá Mundo!")
+mensagem("Como vocês estão?")
+mensagem("https://www.cursoemvideo.com/course/python-3-mundo-3/aulas/funcoes-em-python/")
