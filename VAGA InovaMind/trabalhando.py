6367e643d0419cb91199aa629efe7cea1db2ba34
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-15 11:29:09 -0300

    Editando...

--- VAGA InovaMind/trabalhando.py
+++ VAGA InovaMind/trabalhando.py
@@ -1,5 +1,6 @@
 def anosTrabalhando(anos):
     print("_" * 50)
+    '''
     for x, y in enumerate(anos):
         if x == 0:
             primeiro = y[0]
@@ -9,8 +10,15 @@ def anosTrabalhando(anos):
                 primeiro = y[0]
             if y[1] > ultimo:
                 ultimo = y[1]
-
+    '''
 
 
 lista = [[1993, 2030], [1980, 2020], [1940, 1990], [2003, 2070]]
-anosTrabalhando(lista)
+anos = {}
+for pessoa in lista:
+    for anos_trabalhados in range(pessoa[0], pessoa[1] + 1):
+        if anos_trabalhados in anos:
+            anos[anos_trabalhados] += 1
+        else: 
+            anos[anos_trabalhados] = 1
+print(anos)
