1b5ed69ef078d9d6f1e4292eb9bee13bff2559eb
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-15 20:11:28 -0300

    Exercício: Voto

---
+++ CursoEmVídeo/Mundo 3/FUNÇÕES/EXERCÍCIOS/FUNÇÕES 2/EXERCÍCIOS/voto.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+def voto(ano):
+    idade = datetime.now().year - ano
+    if idade > 60:
+        return print("VOTO OPICIONAL!")
+    else:
+        if idade < 18:
+            return print("VOTO NEGADO!")
+        if idade >= 18:
+            return print("VOTO OBRIGATÓRIO!")
+    
+
+nasc = int(input("Ano de nascimento: "))
+voto(nasc)
