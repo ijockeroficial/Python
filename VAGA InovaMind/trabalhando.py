fa3b41f69aabc2a52fc84b96f12901abf7ae4e84
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-15 10:46:28 -0300

    Editando ainda...

--- VAGA InovaMind/trabalhando.py
+++ VAGA InovaMind/trabalhando.py
@@ -3,9 +3,12 @@ def anosTrabalhando(anos):
     for x, y in enumerate(anos):
         if x == 0:
             primeiro = y[0]
+            ultimo = y[1]
         else:
             if y[0] < primeiro:
                 primeiro = y[0]
+            if y[1] > ultimo:
+                ultimo = y[1]
 
 
 
