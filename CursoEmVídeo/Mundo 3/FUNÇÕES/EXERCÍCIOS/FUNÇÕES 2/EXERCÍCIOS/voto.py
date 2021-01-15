c75bb442b5cbf7828b9700985a09c0bd57f6f4eb
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-15 20:15:25 -0300

    Exercício: Voto

--- CursoEmVídeo/Mundo 3/FUNÇÕES/EXERCÍCIOS/FUNÇÕES 2/EXERCÍCIOS/voto.py
+++ CursoEmVídeo/Mundo 3/FUNÇÕES/EXERCÍCIOS/FUNÇÕES 2/EXERCÍCIOS/voto.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 def voto(ano):
     idade = datetime.now().year - ano
-    if idade > 60:
+    if idade > 60 or 16 <= idade < 18:
         return print("VOTO OPICIONAL!")
     else:
         if idade < 18:
