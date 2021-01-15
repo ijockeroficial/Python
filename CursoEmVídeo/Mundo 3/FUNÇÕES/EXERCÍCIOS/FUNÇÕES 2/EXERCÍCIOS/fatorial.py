4a49a84ba24bb6634b80d11ae14ede05c78eb6a5
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-01-15 20:26:21 -0300

    Exercício: Fatorial

---
+++ CursoEmVídeo/Mundo 3/FUNÇÕES/EXERCÍCIOS/FUNÇÕES 2/EXERCÍCIOS/fatorial.py
@@ -0,0 +1,20 @@
+def fatorial(n, show=False):
+    '''
+    Calcula o fatorial de um numero.
+    Uso: fatorial(numero, show=True)
+    Param n: e o numero que vc quer descobrir o fatorial.
+    Param show: (opcional) Mostrar ou nao a conta.
+    return: O valor do Fatorial de um número n.
+    '''
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end=" ")
+            if c > 1:
+                print(' x ', end=" ")
+            else:
+                print(' = ', end=" ")
+        f *= c
+    return f
+print(fatorial(5, show=True))
+help(fatorial)
